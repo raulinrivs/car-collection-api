32445910048ceb6daa3078168adb6b83a279c9b5
Mateus Raulino <mr.mraulino@gmail.com>
2021-07-02 20:19:41 -0300

    Finalização

--- car-collection-api/src/main/java/br/edu/usj/ads/pw/carcollectionapi/CarController.java
+++ car-collection-api/src/main/java/br/edu/usj/ads/pw/carcollectionapi/CarController.java
@@ -3,11 +3,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 @ResponseBody
+@CrossOrigin
 public class CarController {
     
     @Autowired
@@ -18,4 +24,23 @@ public class CarController {
         List<Car> lista = carRepository.findAll();
         return lista;
     }
+
+    @GetMapping(value="/details/{id}")
+    public Car getCar(@PathVariable Long id) {
+        Car car = new Car();
+        car = carRepository.findById(id).get();
+        return car;
+    }
+
+    @PostMapping(value="/add")
+    public Car addCar(@RequestBody Car car) {
+        Car newCar = carRepository.save(car);
+        return newCar;
+    }
+
+    @GetMapping(value="/delete/{id}")
+    public void getDelete(@PathVariable Long id) {
+        carRepository.deleteById(id);
+    }
+    
 }
